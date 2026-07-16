5de5d4d932f09df52cb8a496c79bc96d23ecec85
Neeko <ddangddooongs@gmail.com>
2026-07-17 06:56:42 +0900

    Initial Commit

---
+++ TomatoDollQuestion/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TomatoDollQuestion
+//
+//  Created by 박정은 on 7/17/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TomatoDollQuestion/TomatoDollQuestionApp.swift
@@ -0,0 +1,17 @@
+//
+//  TomatoDollQuestionApp.swift
+//  TomatoDollQuestion
+//
+//  Created by 박정은 on 7/17/26.
+//
+
+import SwiftUI
+
+@main
+struct TomatoDollQuestionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
